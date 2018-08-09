bb5cbe30219b21bae47e5c2481afcf54dac93cc6
Jai Mitchell <jai@gantic.co>
2018-08-09 21:46:19 +1000

    Initial Gatsby build

---
+++ gatsby-config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Product of ___',
+  },
+  plugins: ['gatsby-plugin-react-helmet'],
+}
---
+++ src/pages/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () => (
+  <div>
+    <h1>Welcome</h1>
+    <p>Product of ___ is an international network of product and design communities.</p>
+    <p>We're working on a website - in the meantime check out our current chapters:</p>
+    <h2>Australia</h2>
+    <ul>
+      <li><a href="https://www.meetup.com/Product-of-BNE/">Brisbane, QLD</a></li>
+      <li><a href="https://www.meetup.com/Product-of-SYD/">Sydney, NSW</a></li>
+      <li><a href="https://www.meetup.com/Product-of-PER/">Perth, WA</a></li>
+      <li>Adelaide, SA <em>(Coming soon)</em></li>
+    </ul>
+    <h2>U.S.A.</h2>
+    <ul>
+      <li><a href="https://www.meetup.com/Product-Of-SFO/">San Francisco, CA</a></li>
+    </ul>
+    <p>If you're in another city and would like to start a chapter, or have any other questions at all, please contact <a href="mailto:jai@gantic.co">Jai Mitchell</a></p>
+  </div>
+)
+
+export default IndexPage
